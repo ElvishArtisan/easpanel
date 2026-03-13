7765922423ab9f44a2967c4827ece9760abc10f9
Fred Gleason <fredg@paravelsystems.com>
2026-03-13 16:24:51 -0400

    2026-03-13 Fred Gleason <fredg@paravelsystems.com> 	* Changed name of the output flatpak bundle from 'easpanel.flatpak' 	to 'com.paravelsystems.easpanel.flatpak'.
    Signed-off-by: Fred Gleason <fredg@paravelsystems.com>

--- make_flatpak.sh
+++ make_flatpak.sh
@@ -3,4 +3,4 @@
 # make_flatpak.sh
 
 flatpak-builder --verbose --force-clean --user --install-deps-from=flathub --repo=repo flatpak com.paravelsystems.easpanel.yml
-flatpak build-bundle repo easpanel.flatpak com.paravelsystems.easpanel
+flatpak build-bundle repo com.paravelsystems.easpanel.flatpak com.paravelsystems.easpanel
