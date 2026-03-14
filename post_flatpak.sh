d87b8e96e5037c58e6a11e3c8e8b09e1650b8d3b
Fred Gleason <fredg@paravelsystems.com>
2026-03-14 10:03:15 -0400

    2026-03-14 Fred Gleason <fredg@paravelsystems.com> 	* Added a 'make flatpak-post' target.
    Signed-off-by: Fred Gleason <fredg@paravelsystems.com>

---
+++ post_flatpak.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+#
+# Check the configuration
+#
+if test -z $FLATPAK_REPO_COLLECTION_ID ; then
+    echo >&2 "No FLATPAK_REPO_COLLECTION_ID value defined, exiting..."
+    exit 1
+fi
+if test -z $FLATPAK_REPO_PATH ; then
+    echo >&2 "No FLATPAK_REPO_PATH value defined, exiting..."
+    exit 1
+fi
+if test -z $FLATPAK_SIGNING_KEY ; then
+    echo >&2 "No FLATPAK_SIGNING_KEY value defined, exiting..."
+    exit 1
+fi
+
+#
+# Do the export
+#
+flatpak build-export \
+	--collection-id=$FLATPAK_REPO_COLLECTION_ID \
+	--gpg-sign=$FLATPAK_SIGNING_KEY \
+	$FLATPAK_REPO_PATH \
+	flatpak
+
+#
+# Post it
+#
+rsync -rptgoDv --copy-links --delete $FLATPAK_REPO_PATH/* $FLATPAK_POST_RSYNC
